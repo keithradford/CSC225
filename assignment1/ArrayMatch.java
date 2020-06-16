15577fa8ac0fef50403783b6414a3fc194313719
keith <khr877@gmail.com>
2020-06-16 14:13:44 -0700

    Initial commit for assignment 1

---
+++ assignment1/ArrayMatch.java
@@ -0,0 +1,86 @@
+/* 
+ * CSC 225 - Assignment 3
+ * Name: 
+ * Student number:
+ */
+ 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.NoSuchElementException;
+
+public class ArrayMatch {
+
+	/*
+	 * match
+	 * Purpose: Determine if the two given arrays 'match'
+	 * Parameters: int[] a, int[] b - the two arrays
+	 * Returns: boolean - true if arrays 'match', false otherwise
+	 * Preconditions: a and b have the same number of elements
+	 */
+	public static boolean match(int[] a, int[] b) {
+		// TODO complete the implementation
+		
+		return false; // change this - set to false so it compiles
+	}
+
+	/*
+	 * fillArray
+	 * Purpose: Fills arrays with contents read from Scanner
+	 * Parameters: int[] x, Scanner fileReader
+	 * Returns: nothing
+	 */
+	public static void fillArray(int[] x, Scanner fileReader) throws NoSuchElementException {
+		Scanner f = new Scanner(fileReader.nextLine());
+		for (int i = 0; i < x.length; i++) {
+			x[i] = f.nextInt();
+		}
+	}
+		
+	/*
+	 * a3Setup
+	 * Purpose: Initializes the input arrays for Assignment 3 match detection
+	 *          by reading data from the text file named fname
+	 * Parameters: String fname - name of the file containig input data
+	 * Returns: nothing
+	 */
+	public static void a3Setup(String fname) {
+		Scanner fileReader = null;
+		int[] A = null;
+		int[] B = null;
+		
+		try {
+			fileReader = new Scanner(new File(fname));
+		} catch (FileNotFoundException e) {
+			System.out.println("Error finding input file");
+			e.printStackTrace();
+			return;
+		}
+		
+		try {
+			int size = Integer.parseInt(fileReader.nextLine());
+			A = new int[size];
+			B = new int[size];
+			fillArray(A, fileReader);
+			fillArray(B, fileReader);
+		} catch (NoSuchElementException e) {
+			System.out.println("Error reading input file data");
+			e.printStackTrace();
+		}
+		
+		if (match(A,B)) {
+			System.out.println("match found");
+		} else {
+			System.out.println("no matches");
+		}
+	}
+	
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("Incorrect usage, should be:");
+			System.out.println("java MysteryArray filename.txt");
+			return;
+		}
+		a3Setup(args[0]);
+	}
+}
