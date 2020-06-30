4474bbe45dcd85de069d329b526bdfe7c4edefde
keith <khr877@gmail.com>
2020-06-30 13:10:46 -0700

    Initial implimentatoin for A4, part 1

--- assignment4/MaxFrequencyHeap.java
+++ assignment4/MaxFrequencyHeap.java
@@ -1,5 +1,5 @@
-// Name: 
-// Student number: V00
+// Name: Keith Radford
+// Student number: V00918086
 
 public class MaxFrequencyHeap implements PriorityQueue {
 	
@@ -18,22 +18,62 @@ public class MaxFrequencyHeap implements PriorityQueue {
 	}
 	
 	public void insert(Entry element) {
-		// TODO: Complete this method
+		int index = this.size;
+		this.size++;
+		this.data[index] = element;
+		if(index > 0){
+			// Bubble up
+			for(int i = index; i > 0; i /= 2){
+				if(this.data[i / 2].getFrequency() < this.data[i].getFrequency()){
+					Entry tmp = this.data[i / 2];
+					this.data[i / 2] = this.data[i];
+					this.data[i] = tmp;
+				}
+				else break;
+			}	
+		}
 	}
 
-	public Entry removeMax() { 
-		// TODO: Complete this method
-		return null; // so it compiles
+	public Entry removeMax() {
+		if(this.size > 0){
+			this.size--;
+			Entry to_ret = this.data[0];
+			if(this.size > 0){
+				// Set first element to last element
+				this.data[0] = this.data[size];
+				// Bubble down
+				int i = 0;
+				while((2 * i) + 1 < size){
+					int right = (2 * i) + 2;
+					int left = (2 * i) + 1;
+					System.out.println("Right" + right + "Left" + left);
+					if(this.data[right].getFrequency() > this.data[left].getFrequency()){
+						Entry tmp = this.data[right];
+						this.data[right] = this.data[i];
+						this.data[i] = tmp;
+						i = right;
+					}
+					else if(this.data[right].getFrequency() < this.data[left].getFrequency()){
+						Entry tmp = this.data[left];
+						this.data[left] = this.data[i];
+						this.data[i] = tmp;
+						i = left;
+					}
+					else break;
+				}
+			}
+			return to_ret;
+		}
+		return null;
 	}
 	
 	public boolean isEmpty() {
-		// TODO: Complete this method
-		return false; // so it compiles
+		if(size != 0) return false;
+		else return true;
 	}
 	
 	public int size() {
-		// TODO: Complete this method
-		return 0; // so it compiles
+		return size;
 	}
 
 }
