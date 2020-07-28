43408cdf7883a333b866bd140757976ce23bc0b3
Keith Radford <khr8771@gmail.com>
2020-07-27 19:11:50 -0700

    Initial commit for assignment 6

---
+++ assignment6/GraphAlgorithms.java
@@ -0,0 +1,39 @@
+/*
+	Name: Keith Radford
+	Student ID: V00918086
+*/ 
+
+import java.awt.Color;
+import java.util.*;
+
+public class GraphAlgorithms{
+
+	/* FloodFillDFS(v, writer, fillColour)
+	   Traverse the component the vertex v using DFS and set the colour 
+	   of the pixels corresponding to all vertices encountered during the 
+	   traversal to fillColour.
+	   
+	   To change the colour of a pixel at position (x,y) in the image to a 
+	   colour c, use
+			writer.setPixel(x,y,c);
+	*/
+	public static void FloodFillDFS(PixelVertex v, PixelWriter writer, Color fillColour){
+		writer.setPixel(v.getX(), v.getY(), fillColour);
+		// TODO: implement this method
+	}
+	
+	/* FloodFillBFS(v, writer, fillColour)
+	   Traverse the component the vertex v using BFS and set the colour 
+	   of the pixels corresponding to all vertices encountered during the 
+	   traversal to fillColour.
+	   
+	   To change the colour of a pixel at position (x,y) in the image to a 
+	   colour c, use
+			writer.setPixel(x,y,c);
+	*/
+	public static void FloodFillBFS(PixelVertex v, PixelWriter writer, Color fillColour){
+		writer.setPixel(v.getX(), v.getY(), fillColour);
+		// TODO: implement this method
+	}
+	
+}
