e877c4f207d39131993ad899c057b28607b6bcae
Keith Radford <khr8771@gmail.com>
2020-07-27 20:40:56 -0700

    Added working DFS colouring algorithm

--- assignment6/GraphAlgorithms.java
+++ assignment6/GraphAlgorithms.java
@@ -8,6 +8,8 @@ import java.util.*;
 
 public class GraphAlgorithms{
 
+	private static LinkedList<PixelVertex> visited = new LinkedList<PixelVertex>();
+
 	/* FloodFillDFS(v, writer, fillColour)
 	   Traverse the component the vertex v using DFS and set the colour 
 	   of the pixels corresponding to all vertices encountered during the 
@@ -19,7 +21,23 @@ public class GraphAlgorithms{
 	*/
 	public static void FloodFillDFS(PixelVertex v, PixelWriter writer, Color fillColour){
 		writer.setPixel(v.getX(), v.getY(), fillColour);
-		// TODO: implement this method
+		visited.add(v);
+
+		LinkedList<PixelVertex> neighbours = v.getNeighbours();
+		int neighbours_len = neighbours.size();
+		int visited_len = visited.size();
+		Boolean new_vertex;
+		for(int i = 0; i < neighbours_len; i++){
+			new_vertex = true;
+			for(int j = 0; j < visited_len; j++){
+				if(neighbours.get(i).equals(visited.get(j))){
+					new_vertex = false;
+				}
+			}
+			if(new_vertex){
+				FloodFillDFS(neighbours.get(i), writer, fillColour);
+			}
+		}
 	}
 	
 	/* FloodFillBFS(v, writer, fillColour)
@@ -33,7 +51,6 @@ public class GraphAlgorithms{
 	*/
 	public static void FloodFillBFS(PixelVertex v, PixelWriter writer, Color fillColour){
 		writer.setPixel(v.getX(), v.getY(), fillColour);
-		// TODO: implement this method
 	}
 	
 }
